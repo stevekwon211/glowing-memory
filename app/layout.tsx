29662fae8d7a82d7557d634053aa07ed2da13d6f
stevekwon211 <stevekwon211@gmail.com>
2024-10-11 14:24:15 +0900

    go

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
+        <html lang="en" className={manrope.className}>
             <body>
                 <AnimatePresence mode="wait">{children}</AnimatePresence>
             </body>
