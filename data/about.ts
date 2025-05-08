8c43cb6a501d8ac3f1e1a353e579d6d9751db250
kwondoeon <doeon@0.space>
2025-05-08 16:27:23 +0900

    Update about.ts

--- data/about.ts
+++ data/about.ts
@@ -1,141 +1,223 @@
 // data/about.ts
 export const aboutContent = {
   title: {
-    en: 'Doeon Kwon',
-    ko: '권도언'
+    en: "Doeon Kwon",
+    ko: "권도언",
   },
   description: {
     en: [
       {
-        type: 'text',
+        type: "text",
         content:
-          "I grew up in Korea, loving the internet, games, history, and stories of great people, which inspired me to dream of starting a software company when I was 15. Driven by the rapidly growing internet and mobile markets in Southeast Asia, I set my sights on building a startup in that region. That's why I chose Australia's "
+          "I grew up in Korea, loving the internet, games, history, and stories of great people, which inspired me to dream of starting a software company when I was 15. Driven by the rapidly growing internet and mobile markets in Southeast Asia, I set my sights on building a startup in that region. That's why I chose Australia's ",
       },
-      { type: 'link', content: 'University of Technology Sydney', url: 'https://www.uts.edu.au/' },
       {
-        type: 'text',
+        type: "link",
+        content: "University of Technology Sydney",
+        url: "https://www.uts.edu.au/",
+      },
+      {
+        type: "text",
         content:
-          ' was close to Southeast Asia and had a solid CS program. I moved there and started studying computer science, but after my first year, I dropped out to join '
+          " was close to Southeast Asia and had a solid CS program. I moved there and started studying computer science, but after my first year, I dropped out to join ",
       },
-      { type: 'link', content: 'Disquiet', url: 'https://disquiet.io/' },
-      { type: 'text', content: " as an early member. Now, I'm co-founding and building " },
-      { type: 'link', content: 'Space Zero', url: 'https://0.space/about' },
+      { type: "link", content: "Disquiet", url: "https://disquiet.io/" },
       {
-        type: 'text',
+        type: "text",
+        content: " as an early member. Now, I'm co-founding and building ",
+      },
+      { type: "link", content: "Space Zero", url: "https://0.space/about" },
+      {
+        type: "text",
         content:
-          ', an AI-powered browser game startup.\n\n2019 ~\n\nWhile on a break from university during my military service in Korea, I started '
+          ", an AI-powered browser game startup.\n\n2019 ~\n\nWhile on a break from university during my military service in Korea, I started ",
       },
-      { type: 'link', content: 'J2KB', url: 'https://stevekwon.notion.site/j2kb' },
       {
-        type: 'text',
+        type: "link",
+        content: "J2KB",
+        url: "https://stevekwon.notion.site/j2kb",
+      },
+      {
+        type: "text",
         content:
-          ', a community to teach coding to non-tech people. It grew to over 500 members in a year, and I secured nonprofit funding. This experience led me to leave university and sparked my interest in startups right away. Around then, I also organized big hackathons like '
+          ", a community to teach coding to non-tech people. It grew to over 500 members in a year, and I secured nonprofit funding. This experience led me to leave university and sparked my interest in startups right away. Around then, I also organized big hackathons like ",
       },
-      { type: 'link', content: 'JunctionX Seoul', url: 'https://www.youtube.com/watch?v=FLZp5-o_zj8' },
-      { type: 'text', content: ' and ' },
-      { type: 'link', content: 'Junction Asia', url: 'https://www.youtube.com/watch?v=yehd_khX-NA' },
       {
-        type: 'text',
+        type: "link",
+        content: "JunctionX Seoul",
+        url: "https://www.youtube.com/watch?v=FLZp5-o_zj8",
+      },
+      { type: "text", content: " and " },
+      {
+        type: "link",
+        content: "Junction Asia",
+        url: "https://www.youtube.com/watch?v=yehd_khX-NA",
+      },
+      {
+        type: "text",
         content:
-          '. As Head of People, I managed hundreds of participants, learned a lot about working as a team, and found great fulfillment in events that supported makers.\n\n2022 ~\n\nAfter sharing my J2KB experiences and thoughts online, I was unexpectedly invited to join Disquiet as its first member. Disquiet was a social network for software builders. We raised about '
-      },
-      { type: 'link', content: '15 million USD in Seed', url: 'https://platum.kr/archives/164398' },
-      { type: 'text', content: ' and ' },
-      { type: 'link', content: 'Pre-A rounds', url: 'https://wowtale.net/2022/11/02/48065/' },
-      {
-        type: 'text',
-        content: ' and reached 130,000 monthly users. Three years later, we stopped the company, and I '
+          ". As Head of People, I managed hundreds of participants, learned a lot about working as a team, and found great fulfillment in events that supported makers.\n\n2022 ~\n\nAfter sharing my J2KB experiences and thoughts online, I was unexpectedly invited to join Disquiet as its first member. Disquiet was a social network for software builders. We raised about ",
       },
       {
-        type: 'link',
+        type: "link",
+        content: "15 million USD in Seed",
+        url: "https://platum.kr/archives/164398",
+      },
+      { type: "text", content: " and " },
+      {
+        type: "link",
+        content: "Pre-A rounds",
+        url: "https://wowtale.net/2022/11/02/48065/",
+      },
+      {
+        type: "text",
+        content:
+          " and reached 130,000 monthly users. Three years later, we stopped the company, and I ",
+      },
+      {
+        type: "link",
         content:
           "learned how vital a founder's drive, curiosity, passion, and a good team are for an early-stage startup",
-        url: 'https://doeon.substack.com/p/reflections-on-disquiet'
+        url: "https://doeon.substack.com/p/reflections-on-disquiet",
       },
       {
-        type: 'text',
+        type: "text",
         content:
-          ".\n\n2025 ~\n\nI'm currently developing Space Zero, an AI-powered multiplayer browser game focused on player freedom. Recently, 600 alpha testers joined in. It's my first time building a game, and I'm combining my lifelong gaming experience with AI to boost both development efficiency and gameplay.\n\nI sometimes write about my experiences and thoughts on "
+          ".\n\n2025 ~\n\nI'm currently developing Space Zero, an AI-powered multiplayer browser game focused on player freedom. Recently, 600 alpha testers joined in. It's my first time building a game, and I'm combining my lifelong gaming experience with AI to boost both development efficiency and gameplay.\n\nI sometimes write about my experiences and thoughts on ",
       },
-      { type: 'link', content: 'Substack', url: 'https://doeon.substack.com/' },
-      { type: 'text', content: ', ' },
-      { type: 'link', content: 'X', url: 'https://x.com/kwondoeon0' },
-      { type: 'text', content: ', and ' },
-      { type: 'link', content: 'Instagram', url: 'https://www.instagram.com/kwondoeon/' },
+      { type: "link", content: "Substack", url: "https://doeon.substack.com/" },
+      { type: "text", content: ", " },
       {
-        type: 'text',
+        type: "link",
+        content: "Linkedin",
+        url: "https://www.linkedin.com/in/kwondoeon/?locale=en_US",
+      },
+      { type: "text", content: ", " },
+      { type: "link", content: "X", url: "https://x.com/kwondoeon0" },
+      { type: "text", content: ", and " },
+      {
+        type: "link",
+        content: "Instagram",
+        url: "https://www.instagram.com/kwondoeon/",
+      },
+      {
+        type: "text",
         content:
-          ". If you're curious about me or want to chat, feel free to reach out at stevekwon211@gmail.com or via social media :)"
-      }
+          ". If you're curious about me or want to chat, feel free to reach out at stevekwon211@gmail.com or via social media :)",
+      },
     ],
     ko: [
       {
-        type: 'text',
+        type: "text",
         content:
-          '저는 인터넷, 게임, 역사, 그리고 위인들의 이야기를 좋아하며 자랐고, 이런 영향으로 중3 때 소프트웨어 창업을 꿈꿨습니다. 인터넷/모바일 시장이 빠르게 성장하는 동남아권에서 창업하고 싶어 근처 호주의 '
-      },
-      { type: 'link', content: '시드니공과대학교', url: 'https://www.uts.edu.au/' },
-      { type: 'text', content: '에서 컴퓨터 과학을 공부했지만, 1학년만 하고 자퇴해 ' },
-      { type: 'link', content: '디스콰이엇', url: 'https://disquiet.io/' },
-      { type: 'text', content: '이라는 팀에 초기 멤버로 합류했었습니다. 지금은 AI 기반 브라우저 게임 스타트업인 ' },
-      { type: 'link', content: 'Space Zero', url: 'https://0.space/about' },
-      { type: 'text', content: '를 공동창업하여 만들고 있습니다.\n\n2019 ~\n\n대학 휴학 후 한국에서 군 복무 중에 ' },
-      { type: 'link', content: 'J2KB', url: 'https://stevekwon.notion.site/j2kb' },
-      {
-        type: 'text',
-        content:
-          '라는 커뮤니티를 시작했습니다. 이 커뮤니티는 비전공자들에게 코딩을 가르치는 곳이었습니다. 1년 만에 500명 이상의 회원이 모였고, 비영리 자금도 확보했습니다. 이 경험은 저를 대학을 떠나 곧바로 스타트업 창업에 관심을 갖게 만든 계기가 됐습니다. 이때 즈음 '
-      },
-      { type: 'link', content: 'JunctionX Seoul', url: 'https://www.youtube.com/watch?v=FLZp5-o_zj8' },
-      { type: 'text', content: '과 ' },
-      { type: 'link', content: 'Junction Asia', url: 'https://www.youtube.com/watch?v=yehd_khX-NA' },
-      {
-        type: 'text',
-        content:
-          ' 같은 대규모 해커톤도 Organizer로 운영했습니다. 제 역할은 Head of People로 수백 명의 참가자를 관리하는 일이었고 팀으로써 일하는 것을 많이 배우고, 메이커를 돕는 행사에서 많은 보람을 얻었습니다.\n\n2022 ~\n\nJ2KB에서의 경험과 생각을 온라인에 공유한 후, 우연히 Disquiet의 첫 멤버로 함께 하자는 제안을 받았습니다. Disquiet은 소프트웨어 메이커를 위한 소셜 네트워크였습니다. '
-      },
-      { type: 'link', content: 'Seed', url: 'https://platum.kr/archives/164398' },
-      { type: 'text', content: ', ' },
-      { type: 'link', content: 'Pre-A 단계', url: 'https://wowtale.net/2022/11/02/48065/' },
-      {
-        type: 'text',
-        content:
-          '에 약 20억원 정도의 자금을 유치했고, 월간 사용자 13만 명을 돌파했습니다. 3년 후 회사를 멈추었는데, 이를 통해 '
+          "저는 인터넷, 게임, 역사, 그리고 위인들의 이야기를 좋아하며 자랐고, 이런 영향으로 중3 때 소프트웨어 창업을 꿈꿨습니다. 인터넷/모바일 시장이 빠르게 성장하는 동남아권에서 창업하고 싶어 근처 호주의 ",
       },
       {
-        type: 'link',
-        content: '창업자의 의지, 문제에 대한 호기심, 열정, 좋은 팀이 초기 스타트업에서 얼마나 중요한지 배웠습니다',
-        url: 'https://kwondoeon.substack.com/p/3-e43'
+        type: "link",
+        content: "시드니공과대학교",
+        url: "https://www.uts.edu.au/",
       },
       {
-        type: 'text',
-        content:
-          '.\n\n2025 ~\n\n현재 저는 Space Zero라는 AI 기반 멀티플레이어 브라우저 게임을 개발하고 있습니다. 플레이의 자유에 초점을 맞춘 게임으로, 최근 600명의 알파 테스터가 참여했습니다. 제 첫 게임 개발이며, 제 인생의 4분의 1을 갈아넣은 무수한 게임 경험을 기반으로 AI를 활용해 개발 효율과 게임플레이 경험을 높이는 시도를 해보고 있습니다.\n\n가끔 '
+        type: "text",
+        content: "에서 컴퓨터 과학을 공부했지만, 1학년만 하고 자퇴해 ",
       },
-      { type: 'link', content: 'Substack', url: 'https://kwondoeon.substack.com/' },
-      { type: 'text', content: ', ' },
-      { type: 'link', content: 'X', url: 'https://x.com/kwondoeon0' },
-      { type: 'text', content: ', ' },
-      { type: 'link', content: 'Instagram', url: 'https://www.instagram.com/kwondoeon/' },
+      { type: "link", content: "디스콰이엇", url: "https://disquiet.io/" },
       {
-        type: 'text',
+        type: "text",
         content:
-          '에 제 경험과 생각을 글로 씁니다. 혹시 저에게 궁금한 것이 있거나, 같이 얘기 나눠보고 싶은 게 있다면 stevekwon211@gmail.com이나 sns를 통해 연락 주세요 :)'
-      }
-    ]
+          "이라는 팀에 초기 멤버로 합류했었습니다. 지금은 AI 기반 브라우저 게임 스타트업인 ",
+      },
+      { type: "link", content: "Space Zero", url: "https://0.space/about" },
+      {
+        type: "text",
+        content:
+          "를 공동창업하여 만들고 있습니다.\n\n2019 ~\n\n대학 휴학 후 한국에서 군 복무 중에 ",
+      },
+      {
+        type: "link",
+        content: "J2KB",
+        url: "https://stevekwon.notion.site/j2kb",
+      },
+      {
+        type: "text",
+        content:
+          "라는 커뮤니티를 시작했습니다. 이 커뮤니티는 비전공자들에게 코딩을 가르치는 곳이었습니다. 1년 만에 500명 이상의 회원이 모였고, 비영리 자금도 확보했습니다. 이 경험은 저를 대학을 떠나 곧바로 스타트업 창업에 관심을 갖게 만든 계기가 됐습니다. 이때 즈음 ",
+      },
+      {
+        type: "link",
+        content: "JunctionX Seoul",
+        url: "https://www.youtube.com/watch?v=FLZp5-o_zj8",
+      },
+      { type: "text", content: "과 " },
+      {
+        type: "link",
+        content: "Junction Asia",
+        url: "https://www.youtube.com/watch?v=yehd_khX-NA",
+      },
+      {
+        type: "text",
+        content:
+          " 같은 대규모 해커톤도 Organizer로 운영했습니다. 제 역할은 Head of People로 수백 명의 참가자를 관리하는 일이었고 팀으로써 일하는 것을 많이 배우고, 메이커를 돕는 행사에서 많은 보람을 얻었습니다.\n\n2022 ~\n\nJ2KB에서의 경험과 생각을 온라인에 공유한 후, 우연히 Disquiet의 첫 멤버로 함께 하자는 제안을 받았습니다. Disquiet은 소프트웨어 메이커를 위한 소셜 네트워크였습니다. ",
+      },
+      {
+        type: "link",
+        content: "Seed",
+        url: "https://platum.kr/archives/164398",
+      },
+      { type: "text", content: ", " },
+      {
+        type: "link",
+        content: "Pre-A 단계",
+        url: "https://wowtale.net/2022/11/02/48065/",
+      },
+      {
+        type: "text",
+        content:
+          "에 약 20억원 정도의 자금을 유치했고, 월간 사용자 13만 명을 돌파했습니다. 3년 후 회사를 멈추었는데, 이를 통해 ",
+      },
+      {
+        type: "link",
+        content:
+          "창업자의 의지, 문제에 대한 호기심, 열정, 좋은 팀이 초기 스타트업에서 얼마나 중요한지 배웠습니다",
+        url: "https://kwondoeon.substack.com/p/3-e43",
+      },
+      {
+        type: "text",
+        content:
+          ".\n\n2025 ~\n\n현재 저는 Space Zero라는 AI 기반 멀티플레이어 브라우저 게임을 개발하고 있습니다. 플레이의 자유에 초점을 맞춘 게임으로, 최근 600명의 알파 테스터가 참여했습니다. 제 첫 게임 개발이며, 제 인생의 4분의 1을 갈아넣은 무수한 게임 경험을 기반으로 AI를 활용해 개발 효율과 게임플레이 경험을 높이는 시도를 해보고 있습니다.\n\n가끔 ",
+      },
+      {
+        type: "link",
+        content: "Substack",
+        url: "https://kwondoeon.substack.com/",
+      },
+      { type: "text", content: ", " },
+      { type: "link", content: "X", url: "https://x.com/kwondoeon0" },
+      { type: "text", content: ", " },
+      {
+        type: "link",
+        content: "Instagram",
+        url: "https://www.instagram.com/kwondoeon/",
+      },
+      {
+        type: "text",
+        content:
+          "에 제 경험과 생각을 글로 씁니다. 혹시 저에게 궁금한 것이 있거나, 같이 얘기 나눠보고 싶은 게 있다면 stevekwon211@gmail.com이나 sns를 통해 연락 주세요 :)",
+      },
+    ],
   },
   links: [
     {
-      title: 'ig',
-      url: 'https://www.instagram.com/kwondoeon/'
+      title: "ig",
+      url: "https://www.instagram.com/kwondoeon/",
     },
     {
-      title: 'x',
-      url: 'https://x.com/kwondoeon0'
+      title: "x",
+      url: "https://x.com/kwondoeon0",
     },
     {
-      title: 'substack',
-      url: 'https://kwondoeon.substack.com'
-    }
-  ]
+      title: "substack",
+      url: "https://kwondoeon.substack.com",
+    },
+  ],
 };
